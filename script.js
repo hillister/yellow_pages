aff55f0c526f80b9506f04872511efe343c65212
Ridhcheall Doherty <132768875+hillister@users.noreply.github.com>
2025-04-16 16:24:37 +0100

    addded fucntion to loop through names and append to DOM

--- script.js
+++ script.js
@@ -1,23 +1,16 @@
-interface Contact {
-    name: string;
-    email: string;
-    phone: string;
-    category: string;
-};
-
-const contacts: Contact[] = [
+"use strict";
+;
+const contacts = [
     { name: "Nina Grey", email: "nina@example.com", phone: "123456", category: "Friends" },
     { name: "Bob Smith", email: "bob@work.com", phone: "987654", category: "Work" },
     { name: "Alice Zee", email: "alice@cool.com", phone: "111222", category: "Others" }
-  ];
-
-  function renderContact() {
-    const container = document.querySelector(".container")
+];
+function renderContact() {
+    const container = document.querySelector(".container");
     contacts.forEach(contact => {
-        let p = document.createElement('p')
+        let p = document.createElement('p');
         p.innerHTML = contact.name;
-        container?.appendChild(p)
+        container === null || container === void 0 ? void 0 : container.appendChild(p);
     });
-  }
-
-  renderContact()
+}
+renderContact();
